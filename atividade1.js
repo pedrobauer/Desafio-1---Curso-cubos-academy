e762f32cff7f4631180aa6bdd5e67edb02607d2f
unknown <pedrolopesbauer@hotmail.com>
2022-10-14 09:56:04 -0300

    Resolução desafio

---
+++ atividade1.js
@@ -0,0 +1,6 @@
+lista = [1, 2, 3, 4];
+totalCofre = 0;
+for (const i of lista) {
+    totalCofre += i;
+}
+console.log(totalCofre);
---
+++ atividade2.js
@@ -0,0 +1,7 @@
+lista = [2, 3, 4];
+totalCofre = 0;
+for (const i of lista) {
+    totalCofre += i;
+}
+const media = totalCofre / lista.length;
+console.log(media);
