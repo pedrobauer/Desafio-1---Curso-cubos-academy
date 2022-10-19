8d12cae56a8f4b8052fa451f99a56b5dfcc3036d
Pedro Henrique Lopes Bauer <80714241+pedrobauer@users.noreply.github.com>
2022-10-18 21:50:15 -0300

    atualização

--- Cofre-Bugadão.js
+++ Cofre-Bugadão.js
@@ -1,11 +1,26 @@
 //const input = "cubos\ncuggbyos";
 const input = "cubos\newvelrabsocaeln";
 
-let senhas = input.split("\n")
+let texto = input.split("\n");
+let index = 0;
+let palavra = "";
+
+for (letra of texto[1]) {
+    if (letra == texto[0][index]) {
+        index++;
+        palavra += letra;
+    }
+}
+if (palavra == texto[0]) {
+    console.log("SIM");
+} else {
+    console.log("NAO");
+}
+
+
+/*let senhas = input.split("\n")
 let confereOrdem = [];
 if (senhas[0].length <= (10 ** 3) && senhas[1].length <= (10 ** 5)) {
-
-
     for (let i = 0; i < senhas[0].length; i++) {
         for (let j = i; j < senhas[1].length; j++) {
             if (senhas[0][i] == senhas[1][j]) {
@@ -21,7 +36,7 @@ if (senhas[0].length <= (10 ** 3) && senhas[1].length <= (10 ** 5)) {
     }
 } else {
     console.log("NAO");
-}
+}*/
 
 
 
